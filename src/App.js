3d4bcbd4ab2ea0f7039c7ff2d58874e0b742df70
Ji-su-Shin <caujisu2020@gmail.com>
2022-10-07 02:28:17 +0900

    refactor: App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Main from './routes/Main';
+import Login from './routes/Login';
+import Register from './routes/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={Main} />
+          <Route exact path="/sign_in" element={Login} />
+          <Route exact path="/sign_up" element={Register} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
