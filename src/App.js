c78897478da2f0c072a58639f3efa8a01070457a
Ji-su-Shin <caujisu2020@gmail.com>
2022-10-07 04:25:31 +0900

    feat: Create MainPage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from './components/Navbar';
 import Main from './routes/Main';
 import Login from './routes/Login';
 import Register from './routes/Register';
+import MainPage from './components/MainPage';
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
 
+//react-router-dom이 v6이 되면서 Switch -> Routes로 변경됨
+//그리고 v6부터는 component가 아니라 element로 props 내보내야 함.
+//https://lefthanddeveloper.tistory.com/16
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={Main} />
-          <Route exact path="/sign_in" element={Login} />
-          <Route exact path="/sign_up" element={Register} />
+          <Route exact path="/" element={MainPage} />
+          <Route exact path="/sign_in" element={<LoginPage/>} />
+          <Route exact path="/sign_up" element={<RegisterPage/>} />
         </Routes>
       </div>
     </Router>
