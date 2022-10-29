5412e5996755fcb29756a4ca7bee4ceef9d1be81
Ji-su-Shin <caujisu2020@gmail.com>
2022-10-29 19:28:19 +0900

    refactor: Route MyPageComponents

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import EditPicturesPage from './pages/EditPicturesPage';
 import MyPage from "./pages/MyPage";
+import MyInfoPage from "./pages/MyPageComponents/MyInfoPage"
+import VoteListMPage from "./pages/MyPageComponents/VoteListMPage"
+import VoteListPPage from "./pages/MyPageComponents/VoteListPPage"
 
 //react-router-dom이 v6이 되면서 Switch -> Routes로 변경됨
 //그리고 v6부터는 component가 아니라 element로 props 내보내야 함.
@@ -24,6 +27,9 @@ function App() {
           <Route exact path="/sign_in" element={<LoginPage/>} />
           <Route exact path="/sign_up" element={<RegisterPage/>} />
           <Route exact path="/mypage" element={<MyPage/>} />
+          <Route exact path="/mypage/myinfo" element={<MyInfoPage/>} />
+          <Route exact path="/mypage/votelistM" element={<VoteListMPage/>} />
+          <Route exact path="/mypage/votelistP" element={<VoteListPPage/>} />
         </Routes>
       </div>
     </Router>
--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,7 +1,21 @@
+import React, {useState} from 'react';
+import {Link} from "react-router-dom";
+
 function MyPage() {
+    const [sign, setSign] = useState(true)
+
+    const onClick = () => {
+        setSign((prev) => !prev)
+    }
+
     return (
         <div>
             <h2>마이페이지</h2>
+            <ul>
+                <li><Link to="/mypage/myinfo"><button onClick={onClick}>나의 정보</button></Link></li> 
+                <li><Link to="/mypage/votelistM"><button onClick={onClick}>내가 만든 투표</button></Link></li>
+                <li><Link to="/mypage/votelistP"><button onClick={onClick}>내가 참여한 투표</button></Link></li>    
+            </ul>
         </div>
     );
 }
