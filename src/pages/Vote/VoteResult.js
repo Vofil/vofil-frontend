3c2f8885432cde8069bb86f24e48594b5b76d7b7
Ji-su-Shin <caujisu2020@gmail.com>
2022-11-18 09:29:02 +0900

    feat: 투표 결과 페이지 api 확인

--- src/pages/Vote/VoteResult.js
+++ src/pages/Vote/VoteResult.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import {useNavigate, useLocation} from "react-router-dom";
+import {React, useState, useEffect} from 'react'
 // 투표 참여 후 투표 결과 페이지
 // 투표 참여한 사람만 볼 수 있음
 // <접근 경로>
@@ -15,20 +16,72 @@ function VoteResult() {
     const location = useLocation()
     const voteID = location.state.id
 
-    // result api 불러오기
-//    for(var i = 1, i < 5, i++){
-//        axios
-//        .get("api/votes/result", { params:
-//            {
-//                id: voteID,
-//                cnt: i
-//            }
-//
-//        })
-//    }
+    // loading 상태
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    // 사진벼롤 받아온 결과 저장
+    const [re1, setRE1] = useState(0);
+    const [re2, setRE2] = useState(0);
+    const [re3, setRE3] = useState(0);
+    const [re4, setRE4] = useState(0);
+
+
+    // 최신 투표 데이터 저장하기
+    const fetchVoteRe = async () => {
+        //초기화
+        setRE1(0)
+        setRE2(0)
+        setRE3(0)
+        setRE4(0)
+
+        setError(null)
+        setLoading(true)
+
+        for(var i = 1; i < 5; i++){
+            axios
+            .get("api/votes/result", { params:
+                {
+                    id: voteID,
+                    cnt: i
+                }
+            })
+            .then((response) => {
+                if(i == 1){
+                    setRE1(response.data)
+                }
+                else if(i == 2){
+                    setRE2(response.data)
+                }
+                else if(i == 3){
+                    setRE3(response.data)
+                }
+                else{
+                    setRE4(response.data)
+                }
+                console.log('well done!')
+            })
+            .catch((error) => {
+                console.log('An error occurred:', error.response);
+            })
+        }
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        fetchVoteRe();
+    }, []);
 
     return(
-        <h2>결과페이지 입니다</h2>
+        <div>
+            <h2>결과페이지 입니다</h2>
+            <ul>
+                <li>{re1}</li>
+                <li>{re2}</li>
+                <li>{re3}</li>
+                <li>{re4}</li>
+            </ul>
+        </div>
     );
 }
 
