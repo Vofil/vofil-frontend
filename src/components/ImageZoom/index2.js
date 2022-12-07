338a21273afd93e536d8a13c796b0ebec2974b98
Ji-su-Shin <caujisu2020@gmail.com>
2022-12-07 15:38:12 +0900

    사진 확대 축소 구현

--- src/components/ImageZoom/index2.js
+++ src/components/ImageZoom/index2.js
@@ -1,26 +1,70 @@
-import {TransformWrapper, TransformComponent} from "react-zoom-pan-pinch";
+import {React, useRef, useState, Component} from 'react';
+//import {TransformWrapper, TransformComponent} from "react-zoom-pan-pinch";
+import AvatarEditor from 'react-avatar-editor';
 
-function ImageZoom(props) {
-    const {imageToZoom, onImageZoomed} = props;
+class ImageZoom extends Component {
 
-    async function zoomImage(zoom) {
+    //const canvasRef = useRef(null);
+    setEditorRef = (editor) => (this.editor = editor)
+    //const [resize, setResize] = useState(1);
 
+    state={
+        resize: 1
     }
-    /*
-        <옵션 설명>
-        - initialScale: 첫 화면 렌더링시, 확대 디폴트 값
-        - minScale: 최소 축소
-        - maxScale: 최대 확대
-    */
-    return(
-        <TransformWrapper initialScale={1} minScale={1} maxScale={10} >
-            <TransformComponent >
-                <figure>
-                    <img src={imageToZoom}/>
-                </figure>
-            </TransformComponent>
-        </TransformWrapper>
-    );
+
+    getBlurredImage = () => {
+        //const blurredImage = this.editor.getImage().toDataURL();
+        if (this.editor){
+            console.log("ediotor: "+ this.editor.getImage().toDataURL())
+            const canvas = this.editor.getImage().toDataURL();
+            let imageURL;
+            fetch(canvas)
+              .then(res => res.blob())
+              .then(blob => (imageURL = window.URL.createObjectURL(blob)));
+            console.log("blurredImageURL: " + canvas)
+            this.props.onImageZoomed(canvas)
+        }
+    }
+
+    plus = () => {
+        if(this.state.resize < 3){
+            this.setState({
+                resize: this.state.resize + 1
+            });
+            //setResize(resize+0.2)
+        }
+    }
+
+    minus = () => {
+        if(this.state.resize >0.3){
+            this.setState({
+                resize: this.state.resize - 1
+            });
+            //setResize(resize-0.2)
+        }
+    }
+
+
+    render() {
+        const {imageToZoom, onImageZoomed} = this.props;
+        return(
+            <div>
+                <AvatarEditor
+                    ref={this.setEditorRef}
+                    image={imageToZoom}
+                    width={250}
+                    height={250}
+                    scale={this.state.resize}
+                    border={0}
+                />
+                <button onClick={this.plus}>+</button>
+                <button onClick={this.minus}>-</button>
+                <button onClick={this.getBlurredImage}>확대축소</button>
+            </div>
+        );
+    }
+
+
 }
 
 export default ImageZoom;
